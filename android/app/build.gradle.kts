7d145018475efcd7b2aa062ed5c5f879a892ea46
Karan Kalyan Askand <karanaskand222@gmail.com>
2026-08-09 16:34:05 +0000

    fix Android release build and Sansphere app configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,31 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
     // ADD THIS LINE BELOW:
     id("com.google.gms.google-services")
+}
+
+
+android {
+    namespace = "com.example.sansphere_android"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    defaultConfig {
+        applicationId = "com.example.sansphere_android"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 }
